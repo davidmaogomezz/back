f878cedc0c77938f806d470a42dbeb6cf0e8339f
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-06-09 21:26:14 -0500

    create user try

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 AdminUser.create!(email: 'admin@example.com', password: 'password') if Rails.env.development?
 User.create!(email: 'admin@example.com', password: 'password') if Rails.env.development?
 Setting.create_or_find_by!(key: 'min_version', value: '0.0')
+User.create!(email: 'shipit@prueba.com', password: '12345678')
